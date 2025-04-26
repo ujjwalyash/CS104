dd31bdeba1cc52568bab04b075b0c4d8b85e66a1
ujjwalyash <yashujjwal24@gmail.com>
2025-04-26 11:18:13 +0530

    Update Q2_submission.sh

--- Labs/bash/Q2_submission.sh
+++ Labs/bash/Q2_submission.sh
@@ -4,4 +4,17 @@ for file in $(find . -type f -name "*.out");do
 	echo $* >> ${file}
 done
 
+---------------------------------------------------------------------------------------------
 
+#!/bin/bash
+touch files.txt
+find ./ -regex ".*\.out$" > files.txt
+
+cat files.txt
+file=files.txt
+if [ -f "$file" ]; then
+  while read line; do
+  echo "$*" >> "$line"
+  done < $file 
+fi
+rm files.txt
